4adb6a56dc8301420eb3c8df5ac925e0a3f28ead
fizyomustafa <fizyomustafa@gmail.com>
2022-05-22 20:28:06 +0300

    ne

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -11,12 +11,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+# SECRET_KEY = config("SECRET_KEY")
+
+SECRET_KEY="django-insecure-vmzgz$r_u-&slv1^akst5=il&156jboifb6_@-+x0$i4wb0%2@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['fizyomustafa.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
